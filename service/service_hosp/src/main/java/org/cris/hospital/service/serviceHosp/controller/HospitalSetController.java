6c98b862b5713f98e3e0f0ffe35454a985a15bbb
Zhangzhenhua <intna1027@gmail.com>
2023-03-03 17:35:02 +0800

    添加ApiOperation

--- service/service_hosp/src/main/java/org/cris/hospital/service/serviceHosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/org/cris/hospital/service/serviceHosp/controller/HospitalSetController.java
@@ -41,6 +41,7 @@ public class HospitalSetController {
     }
 
     //分页查询
+    @ApiOperation(value = "分页查询")
     @PostMapping("findPageHospSet/{current}/{limit}")
     public Result findPageHospSet(@PathVariable long current,
                                   @PathVariable long limit,
@@ -61,6 +62,7 @@ public class HospitalSetController {
     }
 
     //添医院设置
+    @ApiOperation(value = "添加医院设置")
     @PostMapping("saveHospitalSet")
     public  Result saveHospitalSet(@RequestBody HospitalSet hospitalSet){
         hospitalSet.setStatus(1);
@@ -77,6 +79,7 @@ public class HospitalSetController {
     }
 
     //根据ID获取医院设置
+    @ApiOperation(value = "根据id获取医院设置")
     @GetMapping("getHostSet/{id}")
     public Result getHospSet(@PathVariable Long id){
         HospitalSet hospitalSet = hospitalSetService.getById(id);
@@ -84,6 +87,7 @@ public class HospitalSetController {
     }
 
     //修改医院设置
+    @ApiOperation(value = "修改医院设置")
     @PostMapping("updateHospitalSet")
     public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet){
         boolean flag = hospitalSetService.updateById(hospitalSet);
@@ -96,6 +100,7 @@ public class HospitalSetController {
     }
 
     //批量删除
+    @ApiOperation(value = "批量删除")
     @DeleteMapping("batchRemove")
     public  Result batchRemoveHospitalSet(@RequestBody List<Long> idlist){
         hospitalSetService.removeBatchByIds(idlist);
